01c3386920323a5aacf69ea7be1019afc1644443
HomoHikka <halfideas31@gmail.com>
2022-10-08 02:25:35 +0300

    Update Button props

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,12 +2,17 @@ import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
 import classnames from 'classnames';
 
-function Button({ appearance, children }: ButtonProps): JSX.Element {
+function Button({ appearance, children, className, ...props }: ButtonProps): JSX.Element {
 	return (
-		<button className={classnames(
-			styles.button,
-			styles[appearance]
-		)}>
+		<button className={
+			classnames(
+				className,
+				styles.button,
+				styles[appearance]
+			)
+		}
+			{...props}
+		>
 			{children}
 		</button>
 	);

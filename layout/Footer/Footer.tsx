6d3989d694fb0fd97e758f6925368fa957442e8b
HomoHikka <halfideas31@gmail.com>
2022-10-09 18:14:03 +0300

    Add Footer styles
    - add Footer styles
    - add Layout gap

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,9 +1,14 @@
+import classnames from 'classnames';
 import { FooterProps } from './Footer.props';
+import styles from './Footer.module.css';
+import { P } from '../../components';
 
-export function Footer({ ...props }: FooterProps): JSX.Element {
+export function Footer({ className, ...props }: FooterProps): JSX.Element {
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer className={classnames(className, styles.footer)} {...props}>
+			<P>OwlTop © 2020 - {new Date().getFullYear()} Все права защищены</P>
+			<P><a href="#">Пользовательское соглашение</a></P>
+			<P><a href="#">Политика конфиценциальности</a></P>
+		</footer>
 	);
 }

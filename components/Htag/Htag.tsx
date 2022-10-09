8e3cb61ff8014fc910d96a0a47211f74dece8e4c
HomoHikka <halfideas31@gmail.com>
2022-10-09 11:18:39 +0300

    Update Htag.tsx
    - fix export

--- components/Htag/Htag.tsx
+++ components/Htag/Htag.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { HtagProps } from './Htag.props';
 import styles from './Htag.module.css';
 
-function Htag({ tag, children }: HtagProps): JSX.Element {
+export function Htag({ tag, children }: HtagProps): JSX.Element {
 	return React.createElement(tag, { className: styles[tag] }, children);
 }
-
-export default Htag;

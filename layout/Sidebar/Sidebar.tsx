389a781841c599870b8513425f1b2c9e2a106f0f
HomoHikka <halfideas31@gmail.com>
2022-10-13 14:03:11 +0300

    Add Sidebar styles

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
 import { Menu } from '../Menu/Menu';
 import { SidebarProps } from './Sidebar.props';
+import Logo from '../logo.svg';
+import classnames from 'classnames';
+import styles from './Sidebar.module.css';
 
-export function Sidebar({ ...props }: SidebarProps): JSX.Element {
+export function Sidebar({ className, ...props }: SidebarProps): JSX.Element {
 	return (
-		<div {...props}>
+		<div className={classnames(className, styles.sidebar)} {...props}>
+			<Logo className={styles.logo} />
+			<div>Поиск</div>
 			<Menu />
 		</div>
 	);
